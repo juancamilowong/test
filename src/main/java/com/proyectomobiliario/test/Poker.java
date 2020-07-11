3ec38a3869e56fdc270522fb33b89c13c02cfca8
Juan Camilo Wong <juancamilowongramirez@gmail.com>
2020-07-11 13:56:09 -0500

    Files

---
+++ src/main/java/com/proyectomobiliario/test/Poker.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyectomobiliario.test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Poker {
+
+    private static final int POKER_STRAIGHT_LENGTH = 5;
+    private static final int AS = 14;
+
+    public boolean isPokerStraight(int[] cards) {
+
+        if (!validHand(cards)) {
+            return false;
+        }
+
+        int positionAS = Arrays.stream(cards)
+                .boxed()
+                .collect(Collectors.toList())
+                .indexOf(AS);
+
+        if (positionAS >= 0) {
+            
+            long possibleStraightLenght1 = Arrays.stream(cards).filter(x -> x < 14 && x <= 5).count();
+            
+            if(possibleStraightLenght1 >= 4){                
+                cards[positionAS] = 1;
+                Arrays.sort(cards);
+                boolean flag = isStraight(cards);
+                cards[positionAS] =14;
+                return flag;
+            }
+            
+            long possibleStraightLenght2 = Arrays.stream(cards).filter(x -> x < 14 && x >= 10).count();
+            
+             
+            if(possibleStraightLenght2 >= 4){                
+                Arrays.sort(cards);
+            }            
+            //Valido ultimas posiciones
+            if ((positionAS <= 2) && cards[positionAS + 1] == 2) {
+                cards[positionAS] = 1;
+                boolean flag = isStraight(cards);
+                cards[positionAS] =14;
+                return flag;
+
+            }
+            
+        }
+
+        return isStraight(cards);
+    }
+
+    public boolean isStraight(int[] cards) {
+
+        boolean flag = false;
+        int value = -1;
+        int eval = 1;
+        for (int i = 0; i < cards.length; i++, value++, eval++) {
+
+            if (cards[i] < value || cards[i] > value + 1 || value < 0) {
+                value = cards[i];
+                eval = 1;
+                continue;
+            } else {
+                if (cards[i] != value) {
+                    eval = 1;
+                }
+            }
+
+            if (eval >= POKER_STRAIGHT_LENGTH) {
+                flag = true;
+            }
+        }
+        return flag;
+
+    }
+
+    public boolean validHand(int[] cards){
+        //TODO: cambiar validacion a x < 2
+        Predicate<Integer> limitValues = x -> x < 1 || x > 14;
+
+        //Valido tamaño de la lista o array
+        if (cards == null || cards.length > 7 || cards.length < 5) {
+            return false;   
+        }
+
+        List<Integer> cardsList = Arrays.stream(cards).boxed().collect(Collectors.toList());
+
+        // Valido que los valores del array se encuentren dentro de los limites
+        if (cardsList.stream().anyMatch(limitValues)) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    
+}
---
+++ src/test/java/PokerTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.proyectomobiliario.test.Poker;
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PokerTest {
+    
+    public PokerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+     public void straightTest() {
+         Poker poker = new Poker();
+         int[] test1 = new int[]{2, 3, 4 ,5, 6};
+         int[] test2 = new int[]{14, 5, 4 ,2, 3};
+         int[] test3 = new int[]{7, 7, 12 ,11, 3, 4, 14};
+         int[] test4 = new int[]{7, 3, 2};
+         
+         assertTrue("escalera " + Arrays.toString(test1), poker.isPokerStraight(test1));
+         assertTrue("escalera " + Arrays.toString(test2), poker.isPokerStraight(test2));
+         assertFalse("no es escalera " + Arrays.toString(test3),poker.isPokerStraight(test3));
+         assertFalse("no es escalera " + Arrays.toString(test3),poker.isPokerStraight(test4));
+     }
+}
